9e808232271b9809f4eb18a6dab052a3d05873be
umarbashirr <umarbashir93@outlook.com>
2025-04-01 20:01:00 +0530

    Add new columns to Property table in migration.sql for enhanced property details management

---
+++ prisma/migrations/20250401142304_init/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Property" ADD COLUMN     "GSTIN" TEXT,
+ADD COLUMN     "address" TEXT,
+ADD COLUMN     "beneficiaryName" TEXT,
+ADD COLUMN     "checkInTime" TEXT,
+ADD COLUMN     "checkOutTime" TEXT,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "country" TEXT,
+ADD COLUMN     "currency" TEXT,
+ADD COLUMN     "gstAddress1" TEXT,
+ADD COLUMN     "gstAddress2" TEXT,
+ADD COLUMN     "gstPincode" TEXT,
+ADD COLUMN     "gstState" TEXT,
+ADD COLUMN     "mapURL" TEXT,
+ADD COLUMN     "pincode" TEXT,
+ADD COLUMN     "state" TEXT,
+ADD COLUMN     "website" TEXT;
